a350a85431b42d5dee08859fc6e4f69977a49ecf
mankal-27 <mkalkutagi27@gmail.com>
2021-08-03 18:34:16 +0530

    inital commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 const fetch = require("node-fetch");
 const chalk = require("chalk");
 
-const TOKEN = "";
+const TOKEN = "49a9da196062b6c323181f41e8ee829c195ca748";
 
 function getActiveTasks() {
   fetch("https://api.todoist.com/rest/v1/tasks", {
     headers: {
       Authorization: `Bearer ${TOKEN}`,
     },
-  }).then((res) => res.json());
+  }).then((res) => res.json()
+  .then((data) => console.log(data)));
 }
 
-// getActiveTasks();
+getActiveTasks();
 console.log(chalk.blue("Hello world!"));
