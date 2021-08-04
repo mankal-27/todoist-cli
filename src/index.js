0b7745deea9f416746d7c7e28e2f3ecbe0900088
mankal-27 <mkalkutagi27@gmail.com>
2021-08-04 18:16:32 +0530

    testing

--- src/index.js
+++ src/index.js
@@ -1,16 +1,64 @@
 const fetch = require("node-fetch");
 const chalk = require("chalk");
+const yargs = require('yargs')
 
-const TOKEN = "49a9da196062b6c323181f41e8ee829c195ca748";
+const dotenv = require('dotenv').config()
+
+const TOKEN = process.env.TOKEN;
+const URL = process.env.URL;
 
 function getActiveTasks() {
-  fetch("https://api.todoist.com/rest/v1/tasks", {
+  fetch( URL+"tasks", {
     headers: {
       Authorization: `Bearer ${TOKEN}`,
     },
   }).then((res) => res.json()
-  .then((data) => console.log(data)));
+  .then((data) => console.table(data)));
 }
 
-getActiveTasks();
-console.log(chalk.blue("Hello world!"));
+
+function createTask(){
+  let todo = {
+    "content": "Buy Milk",
+    "due_string": "tomorrow at 12:00",
+    "due_lang": "en",
+    "priority": 4
+  };
+  let header={
+     'Content-Type': 'application/json',
+      "Authorization": `Bearer ${TOKEN}`,
+  }
+  fetch(URL +"tasks",{
+    method:'POST',
+    headers: header,
+    body:JSON.stringify(todo)
+  })
+  console.log("task added")
+}
+
+
+function closeTask(){
+  fetch(URL +"tasks/5044055926/close",{
+    method:'POST',
+    headers: {
+      Authorization: `Bearer ${TOKEN}`,
+    },
+  })
+}
+
+function deleteTask(){
+  fetch(URL +"tasks/5044119733",{
+    method:'DELETE ',
+    headers: {
+      Authorization: `Bearer ${TOKEN}`,
+    },
+  })
+}
+
+//createTask()
+//closeTask()
+deleteTask()
+const argv = require('yargs')
+argv.command("ls" , "Get Active Task" , getActiveTasks)
+argv.command("add-task" , "Create a Task",createTask)
+.argv
